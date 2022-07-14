7117920869ca9882c12c24ebcdfa6e211c532916
Gabriel Chiconi <gabriel@lins.dev>
2022-07-14 17:26:04 -0300

    Initialize project

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HelloWorld = () => {
+  return <p className="text-sky-400 font-bold underline">Hello world!</p>;
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import "@styles/globals.css";
+import { AppProps } from "next/app";
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
