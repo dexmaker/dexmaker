2f97716b1756aa7b68c83d7fd6c56423dd70ca06
Gabriel Chiconi <gabriel@lins.dev>
2022-07-19 14:28:37 -0300

    feat: improve gauge animation

--- components/StatGauge.tsx
+++ components/StatGauge.tsx
@@ -12,6 +12,11 @@ export const StatGauge: FC<{ value: number }> = ({ value }) => {
           width: `${(value * 100) / 255}%`,
           backgroundColor: `hsl(${toHue(value)}, 100%, 45%)`,
         }}
+        initial={{
+          width: "0%",
+          backgroundColor: `hsl(0, 100%, 45%)`,
+        }}
+        transition={{ ease: "easeInOut", duration: 0.3 }}
       />
     </span>
   );
