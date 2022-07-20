a9373ef905dbc9f75ff2c49a6c955f2722ce6650
Gabriel Chiconi <gabriel@lins.dev>
2022-07-20 14:42:57 -0300

    content: add Coromon preset

--- data/presets/index.ts
+++ data/presets/index.ts
@@ -1,4 +1,5 @@
 import gen1dex from "./gen1";
+import { coromonDex } from "./coromon";
 import { Dex } from "@data/types";
 
-export const dexes: Dex[] = [gen1dex];
+export const dexes: Dex[] = [gen1dex, coromonDex];
